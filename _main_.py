392d9bdfcadd196f51cd29ca8c0f4da734a81d28
huangchaoming <huangchaoming@bigo.sg>
2018-04-20 19:47:49 +0800

    修改命令行参数，增加五种常见测试场景

--- _main_.py
+++ _main_.py
@@ -7,48 +7,75 @@ import uiautomator2 as u2
 import utils
 from info import CPUInfo, MemInfo, FPSInfo, NetInfo
 from info_task import InfoTask
+from regular_task import RecordVideoTask, MainListScrollTask, DetailsListScrollTask, LoginTask, OpenPermissionsTask
 from task import RandomTask
 
 
 def main(task):
+    task_name = None
     device = None
     applicationid = None
     interval = 1
     duration = 10
     info_names = None
-    output = None
-    opts, args = getopt.getopt(sys.argv[1:], "d:p:f:t:i:o:")
+    output = "."
+    account = None
+    password = None
+    opts, args = getopt.getopt(sys.argv[1:], "t:s:a:f:d:i:o:u:p:")
 
     for opt, value in opts:
-        if opt == "-d":
+        if opt == "-t":
+            task_name = value
+        elif opt == "-s":
             device = value
-        elif opt == "-p":
+        elif opt == "-a":
             applicationid = value
         elif opt == "-f":
             interval = float(value)
-        elif opt == "-t":
+        elif opt == "-d":
             duration = float(value)
         elif opt == "-i":
             info_names = re.split("\s?,\s?", value)
         elif opt == "-o":
-            if value is None:
-                output = "."
-            else:
+            if value is not None:
                 output = value
+        elif opt == "u":
+            account = value
+        elif opt == "p":
+            password = value
 
     d = u2.connect(device)
     pid = utils.get_pid_by_applicationid(d, applicationid)
     version_name = utils.get_version_name_by_applicationid(d, applicationid)
 
+    if task is None:
+        if task_name == "RecordVideo":
+            task = RecordVideoTask(task_name)
+        elif task_name == "MainListScroll":
+            task = MainListScrollTask(task_name)
+        elif task_name == "DetailsListScroll":
+            task = DetailsListScrollTask(task_name)
+        elif task_name == "Login":
+            task = LoginTask(task_name, account, password)
+        elif task_name == "OpenPermissions":
+            task = OpenPermissionsTask(task_name)
+        else:
+            task_name = "Random"
+            task = RandomTask(task_name)
+
+    if task_name == "RecordVideo" or task_name == "MainListScroll" or "DetailsListScroll":
+        task.info_list.add(CPUInfo(task_name, d, device, applicationid, version_name, pid, interval, output))
+        task.info_list.add(MemInfo(task_name, d, device, applicationid, version_name, pid, interval, output))
+
     for info in info_names:
         if info == "cpu":
-            task.info_list.add(CPUInfo(task, d, device, applicationid, version_name, pid, interval, output))
+            task.info_list.add(CPUInfo(task_name, d, device, applicationid, version_name, pid, interval, output))
         elif info == "mem":
-            task.info_list.add(MemInfo(task, d, device, applicationid, version_name, pid, interval, output))
+            task.info_list.add(MemInfo(task_name, d, device, applicationid, version_name, pid, interval, output))
         elif info == "fps":
-            task.info_list.add(FPSInfo(task, d, device, applicationid, version_name, pid, interval, output))
+            task.info_list.add(FPSInfo(task_name, d, device, applicationid, version_name, pid, interval, output))
         elif info == "net":
-            task.info_list.add(NetInfo(task, d, device, applicationid, version_name, pid, interval, output))
+            task.info_list.add(NetInfo(task_name, d, device, applicationid, version_name, pid, interval, output))
 
     task.set_device(d)
     if isinstance(task, RandomTask):
@@ -59,4 +86,4 @@ def main(task):
 
 
 if __name__ == '__main__':
-    main(RandomTask("Random"))
+    main(None)
