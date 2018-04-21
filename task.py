b60aa51bc3dda7680063c66ca8474dfbd5924c32
huangchaoming <huangchaoming@bigo.sg>
2018-04-21 19:12:24 +0800

    统一存放由命令行参数转化的属性

--- task.py
+++ task.py
@@ -5,6 +5,12 @@ class Task(object):
     def __init__(self, name):
         self.name = name
         self.d = None
+        self.device = None
+        self.applicationid = None
+        self.version_name = None
+        self.pid = None
+        self.interval = None
+        self.output = None
         self.info_list = set([])
 
     def execute(self):
@@ -12,6 +18,7 @@ class Task(object):
 
     def add_info(self, info):
         self.info_list.add(info)
+        info.task = self
 
     def set_device(self, d):
         self.d = d
